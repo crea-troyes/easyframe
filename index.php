33da118bdacdcf40aee540e0402488fcb2b7ee0f
Creatroyes <creatroyes@gmail.com>
2020-11-27 13:23:24 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+/***********************************************************************
+
+Version 1.03 
+Start : 02.11.16
+Last : 27.20.20
+
+PHP 7 / HTML5 / CSS3
+
+Bootstrap 4.5 
+
+Développer by créa-troyes.fr  /  @crea_troyes
+
+Documentation : http://easyframe.crea-troyes.fr
+
+***********************************************************************/
+
+// LOADER
+require_once('setting.php');
+require_once('root.php');
+require_once('controleur.php');
+
+?>
